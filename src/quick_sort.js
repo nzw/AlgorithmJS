b2bdc19f533d33832a9706b2b9a3bdfb51e12c67
nzw <info@nzw.me>
2018-10-04 01:05:50 +0900

    bugfix

--- src/quick_sort.js
+++ src/quick_sort.js
@@ -4,7 +4,6 @@ class QuickSort {
 
 	constructor(data_max_num ) {
 		let sort = new Array(data_max_num);
-<<<<<<< HEAD
 		for (let i = 0; i < data_max_num; i++) {
 			sort[i] = Math.ceil(Math.random()*data_max_num);
 		}
@@ -28,35 +27,6 @@ class QuickSort {
 				upper--;
 			}
 			console.log(`lower(${lower}) : upper(${upper})`);
-=======
-		console.log('ソート準備');
-		for (let i = 0; i < data_max_num; i++) {
-			sort[i] = Math.ceil(Math.random()*data_max_num);
-			console.log('sort[i]', sort[i]);
-		}
-		console.log('ソート開始');
-		this.sort(0, data_max_num - 1, sort);
-		console.log('ソート終了');
-		for (let i = 0; i < data_max_num; i++) {
-			console.log('sort[i]', sort[i]);
-		}
-	}
-	sort(bottom, top, data) {
-		let lower = bottom, upper = top, div = 0, tmp = 0;
-		if (bottom >= top) {
-			return;
-		}
-		div = data[bottom];
-		while (lower < upper) {
-			while (lower <= upper && data[lower] <= div) {
-				lower++;
-				console.log('lower => ', lower);
-			}
-			while (lower <= upper && data[upper] > div) {
-				upper--;
-				console.log('upper => ', upper);
-			}
->>>>>>> origin/master
 			if (lower < upper) {
 				tmp = data[lower];
 				data[lower] = data[upper];
@@ -71,9 +41,4 @@ class QuickSort {
 		this.sort(upper + 1, top, data);
 	}
 }
-<<<<<<< HEAD
 new QuickSort(10);
-=======
-
-let quick = new QuickSort(10);
->>>>>>> origin/master
